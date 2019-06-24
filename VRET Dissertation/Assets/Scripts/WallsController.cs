ca825d9825faa2c25818c026c8c4592011f76b87
fredta81 <freddyta99@gmail.com>
2019-06-24 17:36:02 +0100

    Walls now move in and out

--- VRET Dissertation/Assets/Scripts/WallsController.cs
+++ VRET Dissertation/Assets/Scripts/WallsController.cs
@@ -8,6 +8,27 @@ public class WallsController : MonoBehaviour {
     private bool deactivating = false;
     private bool activating = false;
 
+    public GameObject frontWall;
+    public GameObject leftWall;
+    public GameObject rightWall;
+    public GameObject backWall;
+    public GameObject ceiling;
+
+    public float frontWallSpeed;
+    public float sideWallsSpeedMultiplier;
+    public float backWallSpeedMultiplier;
+    public float ceilingSpeedMultiplier;
+
+    private float originalFrontWallPosition;
+    //private float originalLeftWallPosition;
+    //private float originalRightWallPosition;
+    //private float originalBackWallPosition;
+
+    public float endFrontWallPosition;
+    //public float endlLeftWallPosition;
+    //public float endRightWallPosition;
+    //public float endBackWallPosition;
+
     public GameObject windowControllerObject;
 
     private WindowController windowController;
@@ -20,23 +41,40 @@ public class WallsController : MonoBehaviour {
         //Attached to this gameobject
         holeController = gameObject.GetComponent<HoleControllerMaster>();
         textDisplayController = gameObject.GetComponent<TextDisplayController>();
+
+        //Cache positions of each wall so we know how far out they should move
+        originalFrontWallPosition = frontWall.transform.position.z;
+        //originalLeftWallPosition = leftWall.transform.position.x;
+        //originalRightWallPosition = rightWall.transform.position.x;
+        //originalBackWallPosition = backWall.transform.position.z;
     }
 
 	// Update is called once per frame
 	void Update () {
-        if (activated && holeController.CheckAllObjectsStationary()) {
-
-            if (deactivating && windowController.isWindowOpen()) {
+        if (activated) {
+            if (deactivating && windowController.isWindowOpen() && holeController.CheckAllObjectsStationary()) {
                 //If the controller is deactivating, and all objects are stationary, it must be finished deactivating
                 activated = false;
                 deactivating = false;
                 Debug.Log("Walls Controller deactivated");
             }
             //If the objects are all hidden below and the window is closed 
-            else if (holeController.CheckAllObjectsStationary() && windowController.isWindowClosed()) {
-
-                if (Input.GetKey(KeyCode.DownArrow)) {
-                    Debug.Log("Moving walls in");
+            else {
+                //Moving walls in
+                if (Input.GetKey(KeyCode.DownArrow) && frontWall.transform.position.z > endFrontWallPosition) {
+                    frontWall.transform.Translate(0, 0, -frontWallSpeed * Time.deltaTime, Space.World);
+                    leftWall.transform.Translate(frontWallSpeed * sideWallsSpeedMultiplier * Time.deltaTime, 0, 0, Space.World);
+                    rightWall.transform.Translate(frontWallSpeed * -sideWallsSpeedMultiplier * Time.deltaTime, 0, 0, Space.World);
+                    backWall.transform.Translate(0, 0, frontWallSpeed * backWallSpeedMultiplier * Time.deltaTime, Space.World);
+                    ceiling.transform.Translate(0, frontWallSpeed * -ceilingSpeedMultiplier * Time.deltaTime, 0, Space.World);
+                }
+                //Moving walls back out
+                else if (Input.GetKey(KeyCode.UpArrow) && frontWall.transform.position.z < originalFrontWallPosition) {
+                    frontWall.transform.Translate(0, 0, frontWallSpeed * Time.deltaTime, Space.World);
+                    leftWall.transform.Translate(frontWallSpeed * -sideWallsSpeedMultiplier * Time.deltaTime, 0, 0, Space.World);
+                    rightWall.transform.Translate(frontWallSpeed * sideWallsSpeedMultiplier * Time.deltaTime, 0, 0, Space.World);
+                    backWall.transform.Translate(0, 0, frontWallSpeed * -backWallSpeedMultiplier * Time.deltaTime, Space.World);
+                    ceiling.transform.Translate(0, frontWallSpeed * ceilingSpeedMultiplier * Time.deltaTime, 0, Space.World);
                 }
             }
             
